da6e784d9f4aa1a192e219629a83c7887c00cba5
Sebastian Zuzunaga <seb55381@hotmail.com>
2023-11-19 19:52:00 -0500

    finished menu and added validation

--- Algorithm.py
+++ Algorithm.py
@@ -42,17 +42,39 @@ def are_similar_taste(person1, person2, threshold):
 
 def find_similar_pairs(persons_list, threshold):
     similar_pairs = []
+    unpaired_indices = list(range(len(persons_list)))
 
     for i, person1 in enumerate(persons_list):
-        found_pair = False 
-
         for j, person2 in enumerate(persons_list):
-            if i < j and not found_pair:  
+            if i < j:  
                 if are_similar_taste(person1, person2, threshold):
                     similar_pairs.append((person1['name'], person2['name']))
-                    found_pair = True  
 
-    return similar_pairs
+                    if i in unpaired_indices:
+                        unpaired_indices.remove(i)
+                    if j in unpaired_indices:
+                        unpaired_indices.remove(j)
+
+    return similar_pairs, unpaired_indices
+
+def delete_person(person_name, persons_list):
+    for person in persons_list:
+        if person['name'] == person_name:
+            persons_list.remove(person)
+            print(f"{person_name} removido.")
+            return
+    print(f"{person_name} não encontradod.")
+
+def validate_score(message):
+    while True:
+        try:
+            score = int(input(message))
+            if 1 <= score <= 10:
+                return score
+            else:
+                print("Valor deve estar entre 1 e 10.")
+        except ValueError:
+            print("Input inválido.")
 
 persons_list = [
     {'name': 'Alice', 'preferences': [1, 3, 5, 2, 4, 6]},
@@ -61,7 +83,7 @@ persons_list = [
 ]
 
 
-threshold = 1200
+threshold = 20
 
 while True:
     print('######Temporary Menu#######')
@@ -77,38 +99,45 @@ while True:
     if option == '1':
         new_person = input('Insere nome de trabalhador')
         new_person_scores = []
-        #Score 1
-        score = input('score 1')
+        
+        print("Insira o gosto que o empregado tem pelos seguintes temas (1 a 10):")
+        
+        score = validate_score("Esporte: ")
         new_person_scores.append(score)
-        #Score 2
-        score = input('score 2')
+        
+        score = validate_score("Cinema")
         new_person_scores.append(score)
-        #Score 3
-        score = input('score 3')
+        
+        score = validate_score("Cozinha")
         new_person_scores.append(score)
-        #Score 4
-        score = input('score 4')
+        
+        score = validate_score("Leitura")
         new_person_scores.append(score)
-        #Score 5
-        score = input('score 5')
+        
+        score = validate_score("Jogos")
         new_person_scores.append(score)
-        #Score 6
-        score = input('score 6')
+        
+        score = validate_score("Música")
         new_person_scores.append(score)
 
-        new_person_format = {'name': new_person,'preferences': new_person_scores}
+        new_person_format = {'name': new_person, 'preferences': new_person_scores}
         persons_list.append(new_person_format)
     elif option == '2':
         for person in persons_list:
             print(f"{person['name']}: {person['preferences']}")
     elif option == '3':
-        print("wip")
+        person_to_delete = input("Insira o nome do trabalhador a remover: ")
+        delete_person(person_to_delete, persons_list)
     elif option == '4':
-        persons_list_c = persons_list.copy()
-        similar_pairs = find_similar_pairs(persons_list, threshold)
+        similar_pairs, unpaired_indices = find_similar_pairs(persons_list, threshold)
 
-        print(f"Pairs of employees:")
+        print("Duplas de empregados:")
         for pair in similar_pairs:
-            print(f"{pair[0]} and {pair[1]}")
+            print(f"{pair[0]} e {pair[1]}")
+
+        if len(unpaired_indices) > 0:
+            print("Empregados sem dupla:")
+            for idx in unpaired_indices:
+                print(f"{persons_list[idx]['name']} não conseguiu obter dupla.")
     elif option == '5':
         break
